119d79cceb820f31c78453e70ec17a09f5cc6bd2
Bheema Guguloth <bheemudu.guguloth@travelxp.tv>
2024-07-19 01:34:18 +0530

    feat: initial commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**'
+
+            },
+        ],
+        deviceSizes: [340, 640, 768, 1024, 1200, 1920],
+        formats: ['image/webp'],
+    },
+};
+
+export default nextConfig;
