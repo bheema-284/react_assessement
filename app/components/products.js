a9eeb3ae983f2253b88b52c1f42918f284a8ff76
Bheema Guguloth <bheemudu.guguloth@travelxp.tv>
2024-07-19 17:25:43 +0530

    feat: add product details page

--- app/components/products.js
+++ app/components/products.js
@@ -4,10 +4,13 @@ import { useEffect, useRef, useState } from "react";
 import ProductsList from "./productslist";
 import NewPagination from "./pagination";
 import ComboBox from "./combobox";
+import ProductDetails from "./productdetails";
 const Products = (props) => {
     const { data } = props;
     const [searchText, setSearchText] = useState("");
     const [sortBy, setSortBy] = useState("Best Seller");
+    const [screen, setScreen] = useState("product");
+    const [productDetails, setProductDetais] = useState({})
     const [pageIndex, setPageIndex] = useState(1);
     const pageLimit = 8;
     const inputRef = useRef();
@@ -54,6 +57,11 @@ const Products = (props) => {
         setSortBy(e)
     }
 
+    const showDetails = (item) => {
+        setScreen("details");
+        setProductDetais(item)
+    }
+
     useEffect(() => {
         if (inputRef.current) {
             inputRef.current.value = searchText;
@@ -63,55 +71,62 @@ const Products = (props) => {
 
     return (
         <div>
-            <div className="flex flex-col gap-4 md:flex-row md:justify-between my-5">
-                <div className="sm:flex sm:items-center sm:flex-auto gap-5">
-                    <h1 className="text-xl font-semibold text-gray-900">
-                        Product List
-                    </h1>
-                    <div>
-                        <div className="relative py-3 border border-gray-300 rounded-md shadow-sm">
-                            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-1">
-                                <MagnifyingGlassIcon className="h-5 fill-slate-400" />
+            {screen === "product" ?
+                <div>
+                    <div className="flex flex-col gap-4 md:flex-row md:justify-between my-5">
+                        <div className="sm:flex sm:items-center sm:flex-auto gap-5">
+                            <h1 className="text-xl font-semibold text-gray-900">
+                                Product List
+                            </h1>
+                            <div>
+                                <div className="relative py-3 border border-gray-300 rounded-md shadow-sm">
+                                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-1">
+                                        <MagnifyingGlassIcon className="h-5 fill-slate-400" />
+                                    </div>
+                                    <input
+                                        type="text"
+                                        placeholder={"Search By Name"}
+                                        name="searchbox"
+                                        className="block w-full rounded-md border-gray-300 pl-8 focus:border-red-500 focus:ring-red-500 sm:text-sm"
+                                        ref={inputRef}
+                                        onKeyDown={onEnterPress}
+                                    />
+                                    <div className="absolute inset-y-0 right-0 flex items-center pr-2">
+                                        <XMarkIcon className="h-4 fill-slate-400 hover:cursor-pointer" onClick={deleteSearch} />
+                                    </div>
+                                </div>
                             </div>
-                            <input
-                                type="text"
-                                placeholder={"Search By Name"}
-                                name="searchbox"
-                                className="block w-full rounded-md border-gray-300 pl-8 focus:border-red-500 focus:ring-red-500 sm:text-sm"
-                                ref={inputRef}
-                                onKeyDown={onEnterPress}
-                            />
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2">
-                                <XMarkIcon className="h-4 fill-slate-400 hover:cursor-pointer" onClick={deleteSearch} />
+                            <div>
+                                <ComboBox
+                                    title={"Sort By"}
+                                    data={["Low to Hight", "High to Low", "Best Seller"]}
+                                    onChange={onSortByChange}
+                                    value={sortBy}
+                                    containerStyle={"w-full"}
+                                />
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <ComboBox
-                            title={"Sort By"}
-                            data={["Low to Hight", "High to Low", "Best Seller"]}
-                            onChange={onSortByChange}
-                            value={sortBy}
-                            containerStyle={"w-full"}
-                        />
-                    </div>
-                </div>
-            </div>
-            {data ? (
-                <>
-                    <ProductsList
-                        data={searchData(currentData)}
-                    />
-                    <NewPagination
-                        containerstyle={"w-full fixed bottom-0"}
-                        totalRecords={data?.length}
-                        recordsPerPage={pageLimit}
-                        currentPage={pageIndex}
-                        paginationClick={paginationClick}
-                    />
-                </>
-            )
-                : null}
+                    {data ? (
+                        <>
+                            <ProductsList
+                                data={searchData(currentData)}
+                                showDetails={showDetails}
+                            />
+                            <NewPagination
+                                containerstyle={"w-full fixed bottom-0"}
+                                totalRecords={data?.length}
+                                recordsPerPage={pageLimit}
+                                currentPage={pageIndex}
+                                paginationClick={paginationClick}
+                            />
+                        </>
+                    )
+                        : null}
+                </div> : <ProductDetails
+                    data={productDetails}
+                    showDetails={showDetails}
+                />}
         </div>
     )
 }
--- app/components/productslist.js
+++ app/components/productslist.js
@@ -2,7 +2,7 @@
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 const ProductsList = (props) => {
-    const { data } = props;
+    const { data, showDetails } = props;
     const Star = ({ filled }) => (
         <svg className={`w-4 h-4 me-1 ${filled ? 'text-orange-500' : 'text-gray-300'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -12,7 +12,7 @@ const ProductsList = (props) => {
         !data || data.length === 0 ? <div className="mt-10">No Data Available</div> :
             <div className={`${(data.length % 2 !== 0 || data.length % 2 == 0) ? "bg-white" : "bg-slate-200"} "bg-white rounded border border-gray-200 shadow-sm overflow-hidden sm:grid sm:grid-cols-4 sm:divide-y-0"`}>
                 {data?.map((item, index) =>
-                    <div key={index} className="group relative bg-white px-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 cursor-pointer border border-gray-200">
+                    <div onClick={() => showDetails(item)} key={index} className="group relative bg-white px-5 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 cursor-pointer border border-gray-200">
                         <Image className="my-5 w-full aspect-[16/9] object-cover" src={item.thumbnail} alt={"image"} width={242} height={136} />
                         <div className="flex justify-between">
                             <h3 className="text-base font-semibold leading-6 text-gray-900 hover:text-red-500">
